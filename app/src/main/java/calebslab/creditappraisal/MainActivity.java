c586017c691c615b9f3661f2025d4f27c6867b75
DESKTOP-4LBBFU2\Kim <jin4332@gmail.com>
2017-11-30 10:00:02 +0900

    171129작업내용

--- app/src/main/java/calebslab/creditappraisal/MainActivity.java
+++ app/src/main/java/calebslab/creditappraisal/MainActivity.java
@@ -1,13 +1,20 @@
 package calebslab.creditappraisal;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.main);
+
+        ActionBar actionbar = getSupportActionBar();
+        Gongtong gongtong = new Gongtong();
+        gongtong.Title_Bar(actionbar);
     }
 }
