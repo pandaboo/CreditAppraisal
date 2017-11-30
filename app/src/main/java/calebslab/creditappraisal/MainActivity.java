0fe1d647457f201d8d2a9f7ddde59f7317260396
DESKTOP-4LBBFU2\Kim <jin4332@gmail.com>
2017-11-30 14:09:05 +0900

    화면 및 자바추가

--- app/src/main/java/calebslab/creditappraisal/MainActivity.java
+++ app/src/main/java/calebslab/creditappraisal/MainActivity.java
@@ -1,9 +1,12 @@
 package calebslab.creditappraisal;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +19,35 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionbar = getSupportActionBar();
         Gongtong gongtong = new Gongtong();
         gongtong.Title_Bar(actionbar);
+
+        // 변수 선언
+        Button btSmsAfter, btSmsNight, btEnd;
+
+        //객체 대입
+        btSmsAfter = (Button) findViewById(R.id.btSmsAfternoon);
+        btSmsNight = (Button) findViewById(R.id.btSmsNight) ;
+        btEnd = (Button) findViewById(R.id.btEnd);
+
+        btSmsAfter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inSmsAfter = new Intent(getApplicationContext(), SmsAftAvrCnt.class);
+                startActivity(inSmsAfter);
+            }
+        });
+
+        btSmsNight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inSmsNight = new Intent(getApplicationContext(), SmsNgtAvrCnt.class);
+                startActivity(inSmsNight);
+            }
+        });
+        btEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
