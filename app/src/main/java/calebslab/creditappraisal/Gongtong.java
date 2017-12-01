b682f221c6a9d818ef639b66d369db7d66e1549c
parkjinho82 <parkjinho82@gmail.com>
2017-12-01 17:23:52 +0900

    박진호 수정건

--- app/src/main/java/calebslab/creditappraisal/Gongtong.java
+++ app/src/main/java/calebslab/creditappraisal/Gongtong.java
@@ -3,6 +3,9 @@ package calebslab.creditappraisal;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import calebslab.creditappraisal.R;
 
 
@@ -10,5 +13,13 @@ public class Gongtong {
     void Title_Bar(ActionBar actionbar) {
         actionbar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         actionbar.setCustomView(R.layout.custom_bar);
+    }
+
+
+    public String getDate() {
+        SimpleDateFormat dFormat = new SimpleDateFormat("yyyy-MM-dd");
+        long mNow = System.currentTimeMillis();
+        Date mDate = new Date(mNow);
+        return dFormat.format(mDate);
     }
 }
--- app/src/main/java/calebslab/creditappraisal/SmsSndRecCnt.java
+++ app/src/main/java/calebslab/creditappraisal/SmsSndRecCnt.java
@@ -1,10 +1,19 @@
 package calebslab.creditappraisal;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class SmsSndRecCnt extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class SmsSndRecCnt extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +23,79 @@ public class SmsSndRecCnt extends AppCompatActivity {
         ActionBar actionbar = getSupportActionBar();
         Gongtong gongtong = new Gongtong();
         gongtong.Title_Bar(actionbar);
+
+        findViewById(R.id.btEnd).setOnClickListener(this);
+
+        getSmsCnt();
+    }
+
+    private void getSmsCnt() {
+
+        int iSndCnt = 0;
+        int iRecCnt = 0;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_SMS}, 1);
+        } else {
+
+            ArrayList arrayList = new ArrayList<Message>();
+            Uri allMessage = Uri.parse("content://sms");
+            String[] projection = new String[]{"_id", "address", "date", "type"};
+
+            Cursor c = getContentResolver().query(allMessage,
+                    projection,
+                    null,
+                    null,
+                    null);
+
+            if(c.getCount() > 0) {
+                while (c.moveToNext()) {
+
+                    Message msg = new Message(); // 따로 저는 클래스를 만들어서 담아오도록 했습니다.
+
+                    long messageId = c.getLong(0);
+                    msg.setMessageId(String.valueOf(messageId));
+
+                    String address = c.getString(1);
+                    msg.setAddress(address);
+
+                    long timestamp = c.getLong(2);
+                    msg.setTimestamp(String.valueOf(timestamp));
+
+                    int type = c.getInt(3);
+                    msg.setType(type);
+
+                    arrayList.add(msg);
+
+                    if (type == 1) iRecCnt += 1;
+                    else if (type == 2) iSndCnt += 1;
+                }
+            }
+            c.close();
+        }
+
+        TextView textView1 = (TextView) findViewById(R.id.textView1);   //기준일자
+        TextView textView2 = (TextView) findViewById(R.id.textView2);   //발신 Text
+        TextView textView3 = (TextView) findViewById(R.id.textView3);   //수신 Text
+
+        Gongtong gongtong = new Gongtong();
+
+        int iSndAvrCnt = iSndCnt / 30;
+        int iRecAvrCnt = iRecCnt / 30;
+
+        textView1.setText("조회기준일 : " + gongtong.getDate());
+        textView2.setText("최근 30일내 일 평균 발신 SMS 건수는 "+iSndAvrCnt+"건 입니다." + "(총건수:" + iSndCnt + ")");
+        textView3.setText("최근 30일내 일 평균 수신 SMS 건수는 "+iRecAvrCnt+"건 입니다." + "(총건수:" + iRecCnt + ")");
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            //닫기
+            case R.id.btEnd:
+                getSmsCnt();
+                break;
+        }
     }
 }
