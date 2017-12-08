590f8ab94ef26ef96f7aacb596105491310d8992
parkjinho82 <parkjinho82@gmail.com>
2017-12-08 10:17:38 +0900

    로직의 class화

--- app/src/main/java/calebslab/creditappraisal/Gongtong.java
+++ app/src/main/java/calebslab/creditappraisal/Gongtong.java
@@ -1,10 +1,12 @@
 package calebslab.creditappraisal;
 
+import android.content.Context;
 import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
-import android.os.Environment;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 
 import com.github.mikephil.charting.charts.PieChart;
@@ -16,22 +18,18 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.text.ParseException;
 import java.util.ArrayList;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Properties;
 
-import calebslab.creditappraisal.R;
 
-
-
-public class Gongtong {
+public class Gongtong extends AppCompatActivity  {
     void Title_Bar(ActionBar actionbar) {
         actionbar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         actionbar.setCustomView(R.layout.custom_bar);
@@ -147,20 +145,21 @@ public class Gongtong {
 
     /**
      * assets에서 properties 파일을 읽는다.
-     * @param am         : context의 AssetManager
+     * @param ctx         : context
      * @param keyStr     : 추출할 key값
      * @param fileName  : 파일명
      * @return data      : key값의 매칭된 value 값
      */
-    public String[] ReadToAssetsProperty(AssetManager am, String keyStr, String fileName) {
+    public String[] ReadToAssetsProperty(Context ctx, String keyStr, String fileName) {
 
         //property 파일
         InputStream is = null;
         File file = null;
         String arrayData[] = null;
+        AssetManager am = ctx.getResources().getAssets();
 
         try {
-            AssetFileDescriptor fileDescriptor = am.openFd("FILE/" + fileName);
+            AssetFileDescriptor fileDescriptor = am.openFd(fileName);
             FileInputStream fis = null;
             fis = fileDescriptor.createInputStream();
 
@@ -198,4 +197,100 @@ public class Gongtong {
         }
     }
 
+    public HashMap<String, BankInOutData> getBankInOutAmt(ArrayList arrayList, String[] ChkNum, String[] ChkInAmtStr, String[] ChkOutAmtStr) {
+
+        HashMap<String, BankInOutData> mapBankInOutData = new HashMap<String, BankInOutData>();
+        SimpleDateFormat ymDateFormat = new SimpleDateFormat("yyyyMM");
+        int iTmpAmt = 0;
+        String sChkStr = "";
+
+        Log.d("JHPARK", "ChkNum.length = " + ChkNum.length);
+
+        if (arrayList.size() > 0) {
+            BankInOutData TmpBankOutData = null;
+
+            for (int index = 0; index < arrayList.size(); index++) {
+                Message messageOut = (Message) arrayList.get(index);
+                //입금 정보 확인해 보기
+
+                int iTmpType = messageOut.getType();
+                if(iTmpType != 1) continue;
+
+                String sTmpAddress = messageOut.getAddress();
+                String sTmpBody = messageOut.getBody();
+                String sTmpYYYYMM = ymDateFormat.format(messageOut.getTimestamp()).toString();
+
+                for (int ii = 0; ii < ChkNum.length; ii++) {
+
+                    if (ChkNum[ii].equals(sTmpAddress)) {
+
+                        //출금 체크문자 정보 SET
+                        for(int idx=0; idx<ChkOutAmtStr.length; idx++) {
+                            if(sTmpBody.indexOf(ChkOutAmtStr[idx]) >= 0) {
+                                sChkStr = ChkOutAmtStr[idx];
+                                iTmpAmt = getAmt(sTmpBody, sChkStr);
+                            }
+                            if(iTmpAmt > 0) {
+                                if (mapBankInOutData.get(sTmpYYYYMM) != null) {
+                                    TmpBankOutData = mapBankInOutData.get(sTmpYYYYMM);
+                                    mapBankInOutData.put(sTmpYYYYMM,new BankInOutData(sTmpYYYYMM, TmpBankOutData.getInAmt() + iTmpAmt, TmpBankOutData.getOutAmt()));
+                                } else {
+                                    mapBankInOutData.put(sTmpYYYYMM,new BankInOutData(sTmpYYYYMM, iTmpAmt, 0));
+                                }
+                                break;
+                            }
+                        }  // 출금정보 체크완료
+
+                        //입금 체크문자 정보 SET
+                        if(iTmpAmt == 0) {          // 출금정보로 입력이 위에서 발생했다면 SKIP
+                            for (int idx = 0; idx < ChkInAmtStr.length; idx++) {
+                                if (sTmpBody.indexOf(ChkInAmtStr[idx]) >= 0) {
+                                    sChkStr = ChkInAmtStr[idx];
+                                    iTmpAmt = getAmt(sTmpBody, sChkStr);
+                                }
+                                if(iTmpAmt > 0) {
+                                    if (mapBankInOutData.get(sTmpYYYYMM) != null) {
+                                        TmpBankOutData = mapBankInOutData.get(sTmpYYYYMM);
+                                        mapBankInOutData.put(sTmpYYYYMM, new BankInOutData(sTmpYYYYMM, TmpBankOutData.getInAmt(), TmpBankOutData.getOutAmt() + iTmpAmt));
+                                    } else {
+                                        mapBankInOutData.put(sTmpYYYYMM,new BankInOutData(sTmpYYYYMM, 0, iTmpAmt));
+                                    }
+
+                                    iTmpAmt = 0;        //초기화
+                                    break;
+                                }
+                            }
+                        }   // 입금정보 체크완료
+                    }
+                }
+            }
+        }
+        return mapBankInOutData;
+    }
+
+    /**
+     *
+     * @param body      : 문자메시지 본문
+     * @param chkStr    : 문자 본문의 입금/출금 여부 체크값
+     * @return Amt
+     */
+    private int getAmt(String body, String chkStr) {
+
+        int iTmpIndexStart = 0;
+        int iTmpIndexEnd = 0;
+        int amt = 0;
+
+        String sTmpBody = "";
+        String sChkStr = "";
+
+        iTmpIndexStart = body.indexOf(chkStr);
+        sTmpBody = body.substring(iTmpIndexStart + chkStr.length());
+        iTmpIndexEnd = sTmpBody.indexOf("원");
+
+        if (isOnlyDigitChk(sTmpBody.substring(0, iTmpIndexEnd).replace(",", "").trim())) {
+            amt = Integer.parseInt(sTmpBody.substring(0, iTmpIndexEnd).replace(",", "").trim());
+        }
+        return amt;
+    }
 }
+
